30839373d83a9c8eb5593784ae3a596613732778
jovanny0915 <jovanny0915@outlook.com>
2026-02-11 21:27:28 -0500

    fix some issues

--- apis/image.py
+++ apis/image.py
@@ -4,6 +4,7 @@ Image API routes
 from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel, HttpUrl
+from typing import Optional
 from io import BytesIO
 from datetime import datetime
 import uuid
